fa12a9b1d52e0c5d3a7dbe4d1f8564233f155dcf
Badrex9 <107649085+Badrex9@users.noreply.github.com>
2024-06-05 11:22:54 -0400

    Add files via upload

--- Autoencoder_train.py
+++ Autoencoder_train.py
@@ -3,7 +3,8 @@ from keras.models import load_model
 from tensorflow.keras import datasets, layers, models
 import tensorflow as tf
 
-Y_label = 13
+Y_label = 12
+Nb_sample = 1000
 
 def train_model(X,Y, model_old, epochs=20):
     from keras import optimizers
@@ -12,28 +13,30 @@ def train_model(X,Y, model_old, epochs=20):
                 metrics=['accuracy'])
     #print(sys.getsizeof(model))
     model_old.fit(X, Y, epochs=epochs, batch_size=256)#, batch_size=65536)
-    model_old.save('./Y_prediction/model_autoencoder_1.h5')
+    model_old.save('../Y_prediction/model_autoencoder_' +str(Y_label)+ '_' + str(Nb_sample)+ '.h5')
     #model.save('../content/drive/MyDrive/Stage sherbrooke/Model/saved_models/model_dh_30_lr_1e-5
     return model_old
 
 
 print("--------------------Chargement des données train--------------------")
-X_input = np.load('./X_input_split_train_n0/X_input_0.npy')
+X_input = np.load('../X_input_split_train_n0/X_input_0.npy')
 d_model = np.shape(X_input)[1]
 for i in range(1, 20):
-    X_input = np.concatenate((X_input, np.load('./X_input_split_train_n0/X_input_'+str(i)+'.npy')))
-Y = np.load('./X_input_split_train_n0/Y.npy')
+    X_input = np.concatenate((X_input, np.load('../X_input_split_train_n0/X_input_'+str(i)+'.npy')))
+Y = np.load('../X_input_split_train_n0/Y.npy')
 print("--------------------Fin du chargement des données--------------------")
 
-
-X_autoencoder = np.load('./X_input_split_train_n0/X_autoencoder_' + str(Y_label) +'.npy')
-Y_autoencoder = np.load('./X_input_split_train_n0/Y_autoencoder_' + str(Y_label) +'.npy')
+if Nb_sample==1000:
+    X_autoencoder = np.load('./generate/X_generate_' + str(Y_label) +'.npy')
+else:
+    X_autoencoder = np.load('./generate/X_generate_' +str(Y_label)+ '_' + str(Nb_sample)+'.npy')
+Y_autoencoder = [Y_label for i in range(Nb_sample)]
 
 #Concat
 X_input = np.concatenate((X_input, X_autoencoder))
 Y = np.concatenate((Y, Y_autoencoder))
 
-model = load_model('./Y_prediction/model_2D_cnn_retrain_11.h5')  #Meilleur modèle
+model = load_model('../Y_prediction/model_2D_cnn_retrain_11.h5')  #Meilleur modèle
 
 print("--------------------Entrainement du modèle--------------------")
 model = train_model(X_input,Y, model, epochs=20)
