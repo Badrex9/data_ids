7dea83ec8aa55451a4b2c40aa8f30d51de8a5476
Badrex9 <107649085+Badrex9@users.noreply.github.com>
2024-06-10 12:51:11 -0400

    Update entrainement_model_n0_total_avg.py

--- entrainement_model_n0_total_avg.py
+++ entrainement_model_n0_total_avg.py
@@ -386,7 +386,7 @@ X_input = np.load('./X_input_split_train_n0/X_input_0.npy')
 d_model = np.shape(X_input)[1]
 for i in range(1, 20):
     X_input = np.concatenate((X_input, np.load('./X_input_split_train_n0/X_input_'+str(i)+'.npy')))
-Y = np.load('./X_input_split_train_n0/Y.npy')
+Y = np.load('./X_input_split_train_n0/Y_WEB_ATTACK.npy')
 print("--------------------Fin du chargement des données--------------------")
 
 
